d25cac680ace6992ff4ba4690509e79c0f4cf4a2
Eric Sanchez <esanchezverges@gmail.com>
2024-08-10 22:14:28 +0200

    Secred number checked by matching

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,23 @@
+use core::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
 fn main() {
-    let _secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Guess the number");
-    println!("Please input the number");
+    let _secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
 
+    println!("Guess the number");
+    println!("Please input the number");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please write a number!");
+
     println!("You guessed: {}", guess);
-    if guess == _secret_number.to_string() {
-        println!("You guessed right")
-    }
-    else {
-        println!("You guessed wrong, the correct answer was {}",_secret_number);
+    match guess.cmp(&_secret_number) {
+        Ordering::Less => {println!("The correct answer was {}",&_secret_number);println!("Too small");},
+        Ordering::Greater => {println!("The correct answer was {}",&_secret_number);println!("Too big");},
+        Ordering::Equal => println!("You win!"),
     }
 }
